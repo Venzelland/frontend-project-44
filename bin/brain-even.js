90898d5836106b1164a9e4d571dfd2630447cf6f
Venzelland <pavielvolodko1997@mail.ru>
2023-02-17 06:56:55 +0300

    random even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,48 +1,5 @@
-import name from '../src/cli.js';
-import salut from './brain-games.js';
-import readlineSync from 'readline-sync';
+#!/usr/bin/env node
 
-salut
+import runGame from '../src/games/even.js';
 
-console.log("Answer \"yes\" if the number is even, otherwise answer \"no\".");
-
-console.log("Question: 15")
-
-const answer = readlineSync.question('Your answer: ');
-
-if(answer === "no") {
-
-	console.log("Correct!")
-
-	console.log("Question: 6")
-	const answertwo = readlineSync.question('Your answer: ');
-
-	if(answertwo === "yes") {
-		console.log("Correct!")
-
-		console.log("Question: 7")
-		const answerthree = readlineSync.question('Your answer: ');
-
-		if(answerthree === "no") {
-
-			console.log("Correct!")
-			console.log("Congratulations, " + name + "!")
-
-		} else {
-
-			console.log("\'" + answerthree + "\' is wrong answer ;(. Correct answer was \'no\'.\n Let's try again, " + name + "!")
-
-		}
-
-	} else {
-
-		console.log("\'" + answertwo + "\' is wrong answer ;(. Correct answer was \'yes\'.\n Let's try again, " + name + "!")
-
-	}
-
-} else {
-
-	console.log("\'" + answer + "\' is wrong answer ;(. Correct answer was \'no\'.\n Let's try again, " + name + "!")
-
-}
-
+runGame();
--- src/random&even.js
+++
@@ -1,4 +0,0 @@
-export const getRandomNumber = (minRange, maxRange) => {
-	const randomNumber = Math.floor(math.random() * (maxRange - minRange) + minRange);
-	return randomNumber;
-};
