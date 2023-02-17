d65d2e02e92a5857c212b6eab9cbc97d379e51ad
Venzelland <pavielvolodko1997@mail.ru>
2023-02-17 07:06:51 +0300

    random calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,47 +1,5 @@
-import name from '../src/cli.js';
-import salut from './brain-games.js';
-import readlineSync from 'readline-sync';
+#!/usr/bin/env node
 
-salut
+import runGame from '../src/games/calc.js';
 
-console.log("What is the result of the expression?");
-
-console.log("Question: 5 + 10")
-
-const answer = readlineSync.question('Your answer: ');
-
-if(answer === "15") {
-
-	console.log("Correct!")
-
-	console.log("Question: 25 - 11")
-	const answertwo = readlineSync.question('Your answer: ');
-
-	if(answertwo === "14") {
-		console.log("Correct!")
-
-		console.log("Question: 25 * 7")
-		const answerthree = readlineSync.question('Your answer: ');
-
-		if(answerthree === "175") {
-
-			console.log("Correct!")
-			console.log("Congratulations, " + name + "!")
-
-		} else {
-
-			console.log("\'" + answerthree + "\' is wrong answer ;(. Correct answer was 175.\n Let's try again, " + name + "!")
-
-		}
-
-	} else {
-
-		console.log("\'" + answertwo + "\' is wrong answer ;(. Correct answer was 14.\n Let's try again, " + name + "!")
-
-	}
-
-} else {
-
-	console.log("\'" + answer + "\' is wrong answer ;(. Correct answer was 15.\n Let's try again, " + name + "!")
-
-}
+runGame();
