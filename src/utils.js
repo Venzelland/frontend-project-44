d5de9ae6de1e700ae2aa403cb801f05431d5938e
Venzelland <pavielvolodko1997@mail.ru>
2023-03-29 23:26:45 +0300

    last

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 const getRandomNumber = (minRange, maxRange) => Math.floor(
-  Math.random() * (maxRange - minRange + 1) + minRange
+  Math.random() * (maxRange - minRange + 1) + minRange,
 );
 
 export default getRandomNumber;
