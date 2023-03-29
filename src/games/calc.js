7ad7462a8f67a7378a75e9b09a813bff9266131c
Venzelland <pavielvolodko1997@mail.ru>
2023-03-29 21:30:59 +0300

    сохранение до автоматического исправления линтера

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,32 +4,28 @@ import { getRandomNumber } from '../utils.js';
 const operationSymbols = ['+', '-', '*'];
 const description = 'What is the result of the expression?';
 
-const getRandomOperationSymbol  = () => operationSymbols[getRandomNumber(0, (operationSymbols.length - 1))];
-
+const getRandomOperationSymbol = () => operationSymbols[getRandomNumber(0, (operationSymbols.length - 1))];
 
 const calcArithmeticOperation = (symbol, firstValue, secondValue) => {
   switch (symbol) {
     case '+': 
-	return firstValue + secondValue;
-
+      return firstValue + secondValue;
     case '-': 
-	return firstValue - secondValue;
-
+      return firstValue - secondValue;
     case '*': 
-	return firstValue * secondValue;
-	
-	default: 
-		throw new Error(`Unknown symbol: '${symbol}'!`);
+      return firstValue * secondValue;
+    default: 
+      throw new Error(`Unknown symbol: '${symbol}'!`);
   }
 };
 
 const getQuestionAndAnswer = () => {
   const randomSymbol = getRandomOperationSymbol(operationSymbols);
   const firstNumber = getRandomNumber(0, 10);
-  const secondNubmer = getRandomNumber(0, 10);
+  const secondNumber = getRandomNumber(0, 10);
 
-  const question = `${firstNumber} ${randomSymbol} ${secondNubmer}`;
-  const correctAnswer = String(calcArithmeticOperation(randomSymbol, firstNumber, secondNubmer));
+  const question = `${firstNumber} ${randomSymbol} ${secondNumber}`;
+  const correctAnswer = String(calcArithmeticOperation(randomSymbol, firstNumber, secondNumber));
 
   return [question, correctAnswer];
 };
