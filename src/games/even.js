7fc72787e5ce30d021a2d87bb599e9f0163727af
Venzelland <pavielvolodko1997@mail.ru>
2023-03-29 20:28:35 +0300

    double quotes in game-even

--- src/games/even.js
+++ src/games/even.js
@@ -2,7 +2,7 @@ import runGame from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
 const isEvenNumber = (number) => number % 2 === 0;
-const description = "Answer 'yes' if the number is even, otherwise answer 'no'";
+const description = "Answer \"yes\" if the number is even, otherwise answer \"no\"";
 
 const getQuestionAndAnswer = () => {
   const question = getRandomNumber(0, 100);
